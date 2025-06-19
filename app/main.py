183cd6b07e711360bb6687f3cdcd896ddaedbdd8
Nikita Benny <nikita.bennyj@gmail.com>
2025-06-19 00:50:44 -0400

    wrote forecast  router

--- app/main.py
+++ app/main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
-from app.routers import watchlist,news,sentiment
+from app.routers import watchlist,news,sentiment,forecast
 
 
 app = FastAPI()
 
 app.include_router(watchlist.router, prefix="/watchlist")
 app.include_router(news.router, prefix="/news")
-app.include_router(sentiment.router, prefix="/news")
+app.include_router(forecast.router, prefix="/forecast")
+app.include_router(sentiment.router, prefix="/sentiment")
+
 
 
 
