6f109c5d4791c447caa12e5d99b4c66fc87c0e6f
Nikita Benny <nikita.bennyj@gmail.com>
2025-08-26 23:41:44 -0400

    Models

---
+++ app/database.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from sqlalchemy import MetaData
+from typing import List
+from typing import Optional
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
+
+engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
+
+from sqlalchemy.orm import DeclarativeBase
+class Base(DeclarativeBase):
+    pass
+
+
+
+metadata_obj = MetaData()
+
+
